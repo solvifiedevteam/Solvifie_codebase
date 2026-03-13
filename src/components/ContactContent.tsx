392c8abb86c4580ba413ae99f5afa220f2557ac6
DINESH KUMAR DHANAPAL <deepweblearners@gmail.com>
2026-03-13 22:17:21 +0530

    fix: replace broken Google Maps embed URL with stable address-query format

--- src/components/ContactContent.tsx
+++ src/components/ContactContent.tsx
@@ -618,7 +618,7 @@ export default function ContactContent() {
           >
             <iframe
               title="Solvifie Consultancy Office Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3886.9535697621415!2d80.15852937583686!3d13.038234813337905!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a52613134907103%3A0x679468087799e03c!2sVenkata%20Balaji%20Apartment!5e0!3m2!1sen!2sin!4v1700000000000!5m2!1sen!2sin"
+              src="https://maps.google.com/maps?q=Venkata+Balaji+Apartment,+Ponniamman+Kovil+Street,+Aalapakkam,+Chennai+600116&t=&z=16&ie=UTF8&iwloc=&output=embed"
               className="w-full h-full border-0 grayscale group-hover:grayscale-0 transition-all duration-700"
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
